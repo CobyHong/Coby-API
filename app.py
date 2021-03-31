57f3d7f550b53b9f8d6d3f6e703a31623db2c924
Coby Hong <cobyhong@gmail.com>
2021-03-30 21:26:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,18 +21,46 @@ def ping():
     return { "resp": "Coby API is online!" }
 
 # basic users http requests.
-@app.route('/users', methods=['GET', 'POST', 'DELETE'])
+@app.route('/users', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def users():
     search_id = request.args.get('id')
+    search_name = request.args.get('name')
+    search_desc = request.args.get('desc')
+
     if request.method == "GET":
-        # return specific user.
         if search_id:
             user = mongoDB_collection.find_one({"_id": ObjectId(search_id)})
-            user["_id"] = str(user["_id"])
-            return { "users": user }
-        # return all users.
+            if user:
+                user["_id"] = str(user["_id"])
+                return { "users": user }, 201
+            return flask.jsonify({"error": "User not found"}), 404
         else:
             users = list(mongoDB_collection.find())
-            for user in users:
-                user["_id"] = str(user["_id"])
-            return { "users": users }
+            if users:
+                for user in users:
+                    user["_id"] = str(user["_id"])
+                return { "users": users }, 201
+            return flask.jsonify({"error": "users list empty"}), 404
+
+    if request.method == "PUT":
+        target_id = {"_id": ObjectId(search_id)}
+        if search_name and search_desc:
+            update_id = { "$set": { "name": search_name, "desc": search_desc } }
+            resp = mongoDB_collection.update_one(target_id, update_id)
+            if resp:
+                return flask.jsonify({"success": "user updated"}), 204
+        return flask.jsonify({"error": "cannot update user"}), 404
+        
+    if request.method == "POST":
+        new_user = request.get_json()
+        resp = mongoDB_collection.insert(new_user)
+        if resp:
+            return flask.jsonify({"success": "user added"}), 204
+        return flask.jsonify({"error": "user insert failed"}), 404
+
+    if request.method == "DELETE":
+        if search_id:
+            resp = mongoDB_collection.remove({"_id": ObjectId(search_id)})
+            if resp['n'] == 1:
+                return flask.jsonify({"success": "user removed"}), 204
+            return flask.jsonify({"error": "user not found"}), 404
