279844423d4de92ee133ba94ed4e6c76c0edaa69
Coby Hong <cobyhong@gmail.com>
2021-03-30 19:07:02 -0700

    isolate issue with mongoDB connection.

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app = flask.Flask(__name__)
 
 # Connecting to MongoDB database with specified collection.
 # enviroment variable to ensure security.
-mongoDB_url = os.environ.get('MONGO_URL')
-mongoDB_connector = pymongo.MongoClient(str(mongoDB_url))
-mongoDB_collection = mongoDB_connector["CobyBase"]["users"]
+# mongoDB_url = os.environ.get('MONGO_URL')
+# mongoDB_connector = pymongo.MongoClient(str(mongoDB_url))
+# mongoDB_collection = mongoDB_connector["CobyBase"]["users"]
 
 @app.route('/', methods=['GET'])
 def ping():
@@ -20,11 +20,12 @@ def ping():
 
 @app.route('/users', methods=['GET'])
 def users():
-    if request.method == 'GET':
-        user = request.args.get('id')
-        return { "users": 1 }
-    else:
-        users= list(mongoDB_collection.find())
-        for user in users:
-            user["_id"] = str(user["_id"])
-        return {"users": users}
+    return { "users": 1 }
+    # if request.method == 'GET':
+    #     user = request.args.get('id')
+    #     return { "users": 1 }
+    # else:
+    #     users= list(mongoDB_collection.find())
+    #     for user in users:
+    #         user["_id"] = str(user["_id"])
+    #     return {"users": users}
