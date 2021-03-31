c59615d9243051dac4968be9b5731f5525105d7c
Coby Hong <cobyhong@gmail.com>
2021-03-30 18:48:16 -0700

    add mongoDB atlas connection

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import flask
+import pymongo
+import os
 
-# accept arguments
+# accept arguments.
 from flask import request
 
+# flask applicaiton.
 app = flask.Flask(__name__)
+
+# Connecting to MongoDB database with specified collection.
+# enviroment variable to ensure security.
+mongoDB_url = os.environ.get('MONGO_URL')
+mongoDB_connector = pymongo.MongoClient(mongoDB_url)
+mongoDB_collection = mongoDB_connector["CobyBase"]["users"]
 
 @app.route('/', methods=['GET'])
 def ping():
@@ -15,4 +24,7 @@ def users():
         user = request.args.get('id')
         return { "users": 1 }
     else:
-        return { "users": 2 }
+        users= list(mongoDB_collection.find())
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return {"users": users}
