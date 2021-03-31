faf8ad5293401ccb241a7738a046eb2e9a175bc0
Coby Hong <cobyhong@gmail.com>
2021-03-30 19:03:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = flask.Flask(__name__)
 # Connecting to MongoDB database with specified collection.
 # enviroment variable to ensure security.
 mongoDB_url = os.environ.get('MONGO_URL')
-mongoDB_connector = pymongo.MongoClient(mongoDB_url)
+mongoDB_connector = pymongo.MongoClient(str(mongoDB_url))
 mongoDB_collection = mongoDB_connector["CobyBase"]["users"]
 
 @app.route('/', methods=['GET'])
